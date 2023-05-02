4fe96d52ca2e8a8c0182524f79f2024c1f64e3c7
KnownScripts <pals.sten@gmail.com>
2023-05-02 19:03:42 +0300

    DrugSystem1

---
+++ ks-menu/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ks-Menu'
+version '2.0.5'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
